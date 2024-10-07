d34906e79026dc87995ee070b44c2e854a30ee80
“ChenHao” <j842671395@gmail.com>
2024-10-07 14:29:39 +0800

    MODIFY: ruff and prettier

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import *
-from fastapi.responses import *
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from routers import weather, warning, discord
 
@@ -13,16 +13,17 @@ app.mount("/css", StaticFiles(directory="../public/css"), name="css")
 app.mount("/images", StaticFiles(directory="../public/images"), name="images")
 app.mount("/view", StaticFiles(directory="view"), name="view")
 
+
 @app.get("/", include_in_schema=False)
-async def index(request: Request):
-	return FileResponse("../static/index.html", media_type="text/html")
+async def static_index(request: Request):
+    return FileResponse("../static/index.html", media_type="text/html")
+
 
 @app.get("/warning", include_in_schema=False)
-async def index(request: Request):
-	return FileResponse("../static/warning.html", media_type="text/html")
+async def static_warning(request: Request):
+    return FileResponse("../static/warning.html", media_type="text/html")
+
 
 @app.get("/county", include_in_schema=False)
-async def index(request: Request):
-	return FileResponse("../static/county.html", media_type="text/html")
-
-
+async def static_county(request: Request):
+    return FileResponse("../static/county.html", media_type="text/html")
